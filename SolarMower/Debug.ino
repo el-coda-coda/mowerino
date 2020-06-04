890e397f8ee664d7f94bdc4809281d5d1ee10dc4
el-coda-coda <66428797+el-coda-coda@users.noreply.github.com>
2020-06-04 16:45:56 +0200

    Add files via upload

---
+++ SolarMower/Debug.ino
@@ -0,0 +1,70 @@
+/*
+  SolarMower Arduino serialDebug
+ */
+
+void serialDebug()
+{
+  Serial.print("VBat= ");
+  Serial.print(VBat);
+  Serial.println(" V");
+
+  Serial.print("IPanel= ");  
+  Serial.print(IPanel);
+  Serial.println(" A");
+  
+  Serial.print("ICut= ");
+  Serial.print(ICut); 
+  Serial.println(" A");
+  
+  Serial.print("BWFR count= ");
+  Serial.println(BWFR_count);
+  Serial.print("BWFL count= ");  
+  Serial.println(BWFL_count);  
+  Serial.print("US_DX=");
+  Serial.print(US_DX.read());
+  Serial.print("   US_SX=");
+  Serial.println(US_SX.read());  
+  
+  Serial.println();
+}  
+
+void LCDdebug()
+{
+    clearLCD();
+    lcdPosition(0,0);
+    switch (mowerStatus)
+    {
+    case 0:
+      LCD.print("CHARGE ");
+      break;
+    case 1:
+      LCD.print("RUN    ");   
+      break;
+    case 2:
+      LCD.print("STUCK  ");
+      break;
+    case 3:
+      LCD.print("SEARCH ");  
+      break;
+    case 4:
+      LCD.print("BATLOW ");  
+      break;
+    case 5:
+      LCD.print("CHR RS ");
+      break;
+    case 6:
+      LCD.print("CUT ER ");
+      break;  
+    }  
+    
+    LCD.print(VBatPC);
+    LCD.print("%  ");
+     
+    lcdPosition(1,0);
+    LCD.print("IC=");
+    LCD.print(ICut);  
+  
+    lcdPosition(1,9);
+    LCD.print("IP=");
+    LCD.print(IPanel);     
+} 
---
+++ SolarMower/TestMotor.ino
@@ -0,0 +1,78 @@
+/*
+  SolarMower Arduino testMotor
+  motor DX forward and reverse
+  motor SX forward and reverse
+  PEN for exit test
+ */
+
+void testMotor()
+{
+  clearLCD();
+  lcdPosition(0,0);
+  LCD.println("Test motor");
+  Serial.println("Motor DX forward...");  
+  lcdPosition(1,0);  
+  LCD.println("Motor DX forward");  
+  digitalWrite(DIRA_pin, LOW); 
+  for (i=0; i<=255; i++)
+  {
+     analogWrite(PWMA_pin, i);
+     delay(5);
+  }  
+  delay(2000);
+  for (i=255; i>=0; i--)
+  {
+     analogWrite(PWMA_pin, i);
+     delay(5);
+  } 
+  delay(1000);  
+  Serial.println("Motor DX reverse..."); 
+  lcdPosition(1,0);  
+  LCD.println("Motor DX reverse");   
+  digitalWrite(DIRA_pin, HIGH); 
+  for (i=0; i<=255; i++)
+  {
+     analogWrite(PWMA_pin, i);
+     delay(5);
+  }  
+  delay(2000);
+  for (i=255; i>=0; i--)
+  {
+     analogWrite(PWMA_pin, i);
+     delay(5);
+  } 
+  delay(1000); 
+  Serial.println("Motor SX forward...");  
+  lcdPosition(1,0);
+  LCD.println("Motor SX forward");  
+  digitalWrite(DIRB_pin, LOW); 
+  for (i=0; i<=255; i++)
+  {
+     analogWrite(PWMB_pin, i);
+     delay(5);
+  }  
+  delay(2000);
+  for (i=255; i>=0; i--)
+  {
+     analogWrite(PWMB_pin, i);
+     delay(5);
+  } 
+  delay(1000);  
+  Serial.println("Motor SX reverse..."); 
+  lcdPosition(1,0);  
+  LCD.println("Motor SX reverse");  
+  digitalWrite(DIRB_pin, HIGH); 
+  for (i=0; i<=255; i++)
+  {
+     analogWrite(PWMB_pin, i);
+     delay(5);
+  }  
+  delay(2000);
+  for (i=255; i>=0; i--)
+  {
+     analogWrite(PWMB_pin, i);
+     delay(5);
+  } 
+  Serial.println();
+  delay(1000);   
+}  
