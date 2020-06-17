3a5eb88deeb9afb92ae4378263554a0d9b401e24
debugger <debugger@MacBook-Pro-di-debugger.local>
2020-06-17 20:03:30 +0200

    file per controllo esc su pin 11.

---
+++ Path/esc_control.h
@@ -0,0 +1,46 @@
+#include <Servo.h>
+
+#define ESC_ATTACH  11
+#define ON_OFF_ESC_PIN 8
+#define ESC_OFF 1000
+#define ESC_ON 1300 //ESC ON PUO' ANDARE DA 1000 A 2000
+int ON_OFF = 1;
+
+Servo esc;
+
+void setup_esc ()
+{
+    delay(1000);
+    esc.writeMicroseconds(1000);
+    delay(5000);
+    esc.writeMicroseconds(1300);
+    delay(3000);
+    esc.writeMicroseconds(1000);
+}
+
+void esc_on()
+{
+    esc.writeMicroseconds(ESC_ON);
+}
+
+void esc_off()
+{
+    esc.writeMicroseconds(ESC_OFF);
+}
+
+void esc_botton_control()
+{
+    if(digitalRead(ON_OFF_ESC_PIN) ==HIGH)
+    {
+        ON_OFF = ON_OFF * -1;
+    }
+    if(ON_OFF < 0)
+    {
+        esc_on();
+    }
+    else
+    {
+        esc_off();
+    }
+    
+}
