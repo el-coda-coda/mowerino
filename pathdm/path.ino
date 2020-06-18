5340aed5f0ea782671a4d4a675cfb4327b827b98
Ivan Codarin <codarini@bellatrix.local>
2020-06-18 12:39:01 +0200

    rename path

---
+++ pathdm/path.ino
@@ -0,0 +1,37 @@
+
+
+
+void setup ()
+{
+
+   // LiquidCrystal_I2C lcd(0x27,20,4);
+   // lcd.init();
+   // lcd.backlight();
+
+   delay(5000);
+   engines_stop();
+   Serial.begin(9600);
+   /*Serial.println(distance(trig1, echo1));*/
+   pinMode(ULTRASONIC1_TRIGGER_PIN, OUTPUT);
+   pinMode(ULTRASONIC1_ECHO_PIN, INPUT);
+   pinMode(pwm_A, OUTPUT);
+   pinMode(dir_A, OUTPUT);
+   pinMode(pwm_B, OUTPUT);
+   pinMode(dir_B, OUTPUT);
+   pinMode(BUTTON_PIN, INPUT);
+   pinMode(13, OUTPUT);
+
+   /*Testing percorsi semplici*/
+   Serial.println("go forward 200");
+   
+   go_forward(200); 
+
+
+}
+
+
+void loop ()
+{
+   
+go_forward(200); 
+}
