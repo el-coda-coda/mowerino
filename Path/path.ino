8f1cb4f89d16f853c72475a128ca12852fa5aa53
Ivan Codarin <codarini@bellatrix.local>
2020-06-18 12:37:11 +0200

    rinomina di cose

--- Path/path.ino
+++ Path/path.ino
@@ -1,23 +1,4 @@
-#include "constants.h"
-#include "logging.h"
-#include "engines.h"
-#include "segments.h"
 
-
-#define BLADE_WIDTH  23
-#define BUTTON_PIN 8
-#define ROBOT_WIDTH 42
-
-
-//motor A
-#define dir_A  2
-#define pwm_A  6
-
-//motor B
-#define dir_B  12
-#define pwm_B  10
-
-#define spazio_curva  28.3
 
 
 void setup ()
